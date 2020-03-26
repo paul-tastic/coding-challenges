e1b5481af7dd736d8f084cf06b63d9030d1dd31e
Paul <29763912+paul-tastic@users.noreply.github.com>
2020-03-26 10:11:55 -0500

    bubble sort fun!

---
+++ Number-Fun/bubble-sort/solution.php
@@ -0,0 +1,31 @@
+<?php
+
+// doing this one on my own initiative, since I am studying data structures and algorithms for fun.
+
+// Challenge: create a bubble sort to sort an array of numbers
+
+
+function bubbleSort($array) {
+    $counter = count($array);
+    while ($counter >= 0) {
+        // iterate through array and compare values of neighbors
+        for ($i=0;$i<$counter-1;$i++) {
+            if ($array[$i] > $array[$i+1]) {
+                // swap position
+                // $t1 = $array[$i+1];
+                // $array[$i+1] = $array[$i];
+                // $array[$i] = $t1;
+                [$array[$i], $array[$i+1]] = [$array[$i+1], $array[$i]];
+            }
+
+        }
+        $counter--;
+    }
+    return $array;
+}
+
+// test cases:
+$inputArr = [3,2,5,7,1,9]; // expected sort output: 1,2,3,5,7,9
+echo "before bubble sort: ".implode(',',$inputArr)."<br>";
+$inputArr = bubbleSort($inputArr);
+echo "after bubble sort: ".implode(',',$inputArr);
